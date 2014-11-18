68dd8d07a0e0275220082b6e99b864129bb57efe
Jonathan Knapp <jon@coffeeandcode.com>
2014-11-18 13:13:12 -0500

    got app working with Marionette v2+ constructor calls and destroy method

--- backbone.marionette.zombies.js
+++ backbone.marionette.zombies.js
@@ -1,4 +1,4 @@
-(function(Backbone, window) {
+(function(Backbone, Marionette, window) {
   'use strict';
 
   var windowProperty = 'zombies';
@@ -6,8 +6,15 @@
     views: new Backbone.Collection()
   };
 
-  var constructor = Backbone.Marionette.View.prototype.constructor;
-  Backbone.Marionette.View.prototype.constructor = function() {
+  var constructor = Marionette.View;
+  var properties = {
+    prototype: Marionette.View.prototype
+  };
+  _.each(_.keys(Marionette.View), function(key) {
+    properties[key] = Marionette.View[key];
+  });
+
+  Marionette.View = function() {
     var result = constructor.apply(this, arguments);
     var attributes = { id: this.cid };
 
@@ -18,10 +25,13 @@
     zombies.views.push(new Backbone.Model(attributes));
     return result;
   };
+  _.each(properties, function(value, key) {
+    Marionette.View[key] = value;
+  });
 
-  var close = Backbone.Marionette.View.prototype.close;
-  Backbone.Marionette.View.prototype.close = function() {
-    var result = close.apply(this, arguments);
+  var destroy = Marionette.View.prototype.destroy;
+  Marionette.View.prototype.destroy = function() {
+    var result = destroy.apply(this, arguments);
     zombies.views.remove(zombies.views.get(this.cid));
     return result;
   };
@@ -31,4 +41,4 @@
   } else {
     console.warn('window.' + windowProperty + ' property already exists, unable to provide access');
   }
-})(Backbone, window);
+})(Backbone, Marionette, window);
